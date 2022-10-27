23dc5c392d2728a2394a0ad9d86764f3ee1cb01a
Mahmoud Khaled <mahmoudkhaled@Mahmouds-MacBook-Pro.local>
2022-10-27 17:23:30 +0200

    Task 55, 56, 57 done v1

--- server/index.js
+++ server/index.js
@@ -6,21 +6,4 @@ const Admins = require("./models/Admin");
 const Instructors = require("./models/Instructor");
 const Trainees = require("./models/Trainee");
 app.listen(port);
-
-app.post("/addAdmin", (req,res)=>{
-    const newAdmin = new Admins(req.body);
-    newAdmin.save().then(() => console.log("Admin added successfully")).catch((err) => console.log(err));
-    res.send("new admin added successfully");
-});
-
-app.post("/addInstructor", (req,res)=>{
-    const newInstructor = new Instructors(req.body);
-    newInstructor.save().then(() => console.log("Instructor added successfully")).catch((err) => console.log(err));
-    res.send("new instructor added successfully");
-});
-
-app.post("/addCorporateTrainee", (req,res)=>{
-    const newCorporateTrainee = new Trainees(req.body);
-    newCorporateTrainee.save().then(() => console.log("Trainee added successfully")).catch((err) => console.log(err));
-    res.send("new corporate trainee added successfully");
-});
+app.use("/admin",require("./routes/api/Admin"))
